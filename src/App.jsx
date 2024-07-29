ae493f58164ed1a2fa1fe9ea34aa7d37c363ff74
“kishlaychandan00” <kishlaychandan00@email.com>
2024-07-30 01:14:59 +0530

    adding image generator

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useRef, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+const token = "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";
+function App() {
+  const[image,setImage]=useState("");
+  
+  async function query(data) {
+    alert("working");
+    const options = {
+      method: "POST",
+      url: "https://ai-text-to-image-generator-api.p.rapidapi.com/realistic",
+      headers: {
+        "x-rapidapi-key": "d5a2d91272msh063705f414bec1fp11963djsn58fae9870b70",
+        "x-rapidapi-host": "ai-text-to-image-generator-api.p.rapidapi.com",
+        "Content-Type": "application/json",
+      },
+      data: {
+        inputs:data,
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      setImage(response.data.url);
+      // console.log(response.data.url);
+      console.log(":done");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  const ref=useRef(null);
+
+  return (
+    <>
+      <div className="main">
+      <h1 className="heading">Image Generator</h1>
+      <input type="text"  ref={ref} placeholder="enter name" />
+      <button onClick={()=>query(ref.current.value)}>Submit</button>
+      {/* <h1>{image}</h1> */}
+      {image? <img src={image} />:null}
+      </div>
+    </>
+  );
+}
+
+export default App;
